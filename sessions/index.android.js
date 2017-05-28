c4edc476cdcf59ce11617b55a301574e4ddf0dc7
myztajay <myztajay@hotmail.com>
2017-05-28 17:21:29 -0400

    Added ios default to Nav component

--- sessions/index.android.js
+++ sessions/index.android.js
@@ -1,13 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-
 import { AppRegistry } from 'react-native';
 import { Nav } from './src/Nav'
-
-
 
 AppRegistry.registerComponent('sessions', () => Nav);
