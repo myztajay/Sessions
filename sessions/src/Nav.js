1ebc34d19c8b8694743ddb1d50b0e5dac01527af
myztajay <myztajay@hotmail.com>
2017-05-28 17:19:34 -0400

    Setup some basic styling on Nav to match the mockup

--- sessions/src/Nav.js
+++ sessions/src/Nav.js
@@ -7,6 +7,22 @@ const Nav = TabNavigator({
   Home: { screen: SessionsScreen },
   Profile: { screen: ProfileScreen },
   Settings: { screen: SettingsScreen }
-})
+}, {
+  tabBarPosition: 'bottom',
+  tabBarOptions: {
+    activeTintColor: 'white',
+  
+    pressOpacity: 0.5,
+    // eventually will be icon
+    // showIcon: true,
+    tabStyle: {
+      backgroundColor: '#87281eff',
+    },
+    style: {
+      backgroundColor: '#c0392bff'
+    }
+  }
+}
+)
 
 export { Nav };
