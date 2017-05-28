caa3fdb644938024a4a2ecf1151a1be68b3d751b
myztajay <myztajay@hotmail.com>
2017-05-28 16:43:03 -0400

    Create empty screen compoenents for Navigator to have screens to work with[D

---
+++ sessions/src/screens/SessionScreen.js
@@ -0,0 +1,15 @@
+import  React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+
+class SessionScreen extends Component{
+  render(){
+    return(
+      <View>
+        <Text> Session Screen </Text>
+      </View>
+    )
+  }
+}
+
+export { SessionScreen };
