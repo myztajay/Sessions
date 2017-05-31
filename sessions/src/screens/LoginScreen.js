9f7ca12e7f32fa0a113c5daa31325041d8c22e5d
myztajay <myztajay@hotmail.com>
2017-05-31 17:02:59 -0400

    Added loginScreen with error checking and state of autheticated which check switches upon successful login

--- sessions/src/screens/LoginScreen.js
+++ sessions/src/screens/LoginScreen.js
@@ -2,30 +2,57 @@ import  React, { Component } from 'react';
 import { firebase } from 'firebase'
 import { View, Text, Button} from 'react-native';
 import { Input, CardSectionVertical, Card } from '../components/common'
-import { Firebase, auth, googleAuthProvider } from '../../Firebase';
+import { Firebase } from '../../Firebase';
 
 
 class LoginScreen extends Component{
   static navigationOptions = {
   title: 'Login'
   }
+
+  constructor(props){
+  super(props)
+    this.state = {
+      email: '',
+      password: '',
+      error: '',
+      authenticated: false
+    }
+  }
+
+  onButtonPress = () => {
+    Firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
+    .then(()=>{
+      this.setState({ authenticated: true })
+      this.props.navigation.navigate('Main')
+    })
+    .catch((error)=>{
+      this.setState({ error: error.message })
+    })
+  }
+
   render(){
     return(
       <View>
         <CardSectionVertical>
           <Input
             label='Email'
+            onChangeText={(email) => this.setState({ email })}
           />
           <Input
             label="Password"
             secureTextEntry={true}
+            onChangeText={(password) => this.setState({ password })}
           />
-          </CardSectionVertical>
-      
+          <Text>{this.state.error}</Text>
+          <Button
+            title='Login'
+            onPress={this.onButtonPress.bind(this)}
+          />
+        </CardSectionVertical>
       </View>
     )
   }
 }
-
 
 export { LoginScreen };
