da6e47377b70b80a9e53282f99fe308f1fc5545a
myztajay <myztajay@hotmail.com>
2017-05-31 19:01:39 -0400

    Added Firebase Writing ability via NewSessionsScreen

--- sessions/src/screens/NewSessionScreen.js
+++ sessions/src/screens/NewSessionScreen.js
@@ -1,15 +1,45 @@
 import  React, { Component } from 'react';
-import { View, Text } from 'react-native';
-
+import { View, Text, Button } from 'react-native';
+import { Firebase } from '../../Firebase'
+import { Input, CardSectionVertical, Card } from '../components/common'
 
 class NewSessionScreen extends Component{
   static navigationOptions = {
   title: 'New Session'
   }
+  constructor(props){
+  super(props)
+    this.state = {
+      name: '',
+      description: '',
+      error: ''
+    }
+  }
+  onCreateButtonPress(){
+    Firebase.database().ref()
+      .child('sessions')
+      .push({name: this.state.name, description: this.state.description})
+      this.props.navigation.navigate('Main')
+  }
+
   render(){
     return(
       <View>
-        <Text> NewSessionScreen </Text>
+        <CardSectionVertical>
+          <Input
+            label='Name'
+            onChangeText={(name) => this.setState({ name })}
+          />
+          <Input
+            label="Description"
+            onChangeText={(description) => this.setState({ description })}
+          />
+          <Text>{this.state.error}</Text>
+          <Button
+            title='Create Session'
+            onPress={this.onCreateButtonPress.bind(this)}
+          />
+        </CardSectionVertical>
       </View>
     )
   }
