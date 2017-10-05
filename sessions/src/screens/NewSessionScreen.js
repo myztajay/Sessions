a1e4296568dfac1bac54f3ef60b17335d53c0971
myztajay <myztajay@hotmail.com>
2017-10-04 20:18:52 -0400

    Added Compoenent that renders suggest place based on API response

--- sessions/src/screens/NewSessionScreen.js
+++ sessions/src/screens/NewSessionScreen.js
@@ -15,7 +15,9 @@ class NewSessionScreen extends Component{
       description: '',
       error: '',
       user:'',
-      location: 'home'
+      location: '',
+      isLocation: false,
+      googlePlace: ''
     }
   }
 
@@ -25,8 +27,7 @@ class NewSessionScreen extends Component{
     if (user){
     Firebase.database().ref()
       .child('sessions')
-      .push({name: this.state.name, description: this.state.description, location: this.state.location, creator: user.email, uid:user.uid })
-
+      .push({name: this.state.name, description: this.state.description, googlePlace: this.state.googlePlace, location: this.state.location, creator: user.email, uid:user.uid })
     this.props.navigation.navigate('Main')}
     else{
       this.props.navigation.navigate('Login')
@@ -34,14 +35,28 @@ class NewSessionScreen extends Component{
   }
   
   onLocationInput(location){
-    console.warn(location)
     this.setState({ location })
     //Trigger API call for google place if above number of characters
     // Need to store response and only do calls if location doesnt match.
     if (location.length >= 9){
       console.warn('enough for api call')
       axios.get(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${location}&key=${AppKey}`)
-      .then((res)=>console.log(res))
+      .then((res)=>{
+        //Sometime will return multiple items so refactor is need for listing multiple choices
+        this.setState({
+          googlePlace: res.data.results[0]
+        })        
+      })
+    }
+  }
+  
+  renderGooglePlace(){
+    if(!this.state.isLocation){
+      return (
+        <Text
+        onPress={()=> this.setState({ location: this.state.googlePlace.formatted_address, isLocation: true })}
+        >{this.state.googlePlace.formatted_address}</Text>
+      ) 
     }
   }
 
@@ -60,7 +75,11 @@ class NewSessionScreen extends Component{
           <Input
             label='Location'
             onChangeText={this.onLocationInput.bind(this)}
+            value={this.state.location}
           />
+          
+          {this.renderGooglePlace()}
+          
           <Text>{this.state.error}</Text>
           <Button
             title='Create Session'
